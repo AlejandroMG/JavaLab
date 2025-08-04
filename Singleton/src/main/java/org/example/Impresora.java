5185f5694f498c78875cf051d4ddfd94612eb804
AlejandroMG <alejandro.fierroc@usm.cl>
2025-08-04 11:41:12 -0400

    Singleton

---
+++ Singleton/src/main/java/org/example/Impresora.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Impresora {
+    private static Impresora instance;
+
+    private Impresora() {}
+
+    public static Impresora getInstance() {
+        if (instance == null) {
+            instance = new Impresora();
+        }
+        return instance;
+    }
+
+    public void ImprimirLine(String texto) {
+        System.out.println(texto);
+    }
+}
